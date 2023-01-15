642d60ec396440baa3fb8c6610f083747289fff6
CatsAndMice <lihai_js@163.com>
2023-01-15 17:55:07 +0800

    chore: shell自动输入密码

--- ssh/index.js
+++ ssh/index.js
@@ -1,33 +1,36 @@
 const { Client } = require('ssh2');
 const conn = new Client();
-conn.on('ready', (err) => {
-    console.log('ready');
-    conn.exec(`set timeout 1
- 
-    ## 切换root用户
-    spawn su root
-     
-    ## 判断上述进程(su kevin)的输出结果中是否有“Password”的字符串(不区分大小写)。
-    ## 若有则立即返回，否则就等待一段时间后返回，等待时长就是开头设置的1秒
-    expect "Password:"
-     
-    ## 向上面的进程(su kevin)发送字符串中的密码, 并且自动敲Enter健(\r)
-    send "ywja666\r"
-     
-    interact
-    `, (err, stream) => {
-        if (err) throw err;
-        stream.on('close', (code, signal) => {
-            conn.exec('systemctl status docker', (err, s) => {
-                s.on('data', (data) => {
-                    console.log(data.toString());
+const execFn = (c = conn) => {
+    return (command) => {
+        return new Promise((resolve, reject) => {
+            c.exec(command, (err, stream) => {
+                if (err) {
+                    reject(err)
+                    return
+                }
+                let result = ''
+                stream.on('close', () => {
+                    resolve(String(result))
+                }).on('data', (data) => {
+                    result += data
                 })
             })
-        }).on('data', (data) => {
-            console.log(data);
         })
-    });
+    }
+}
 
+const exec = execFn(conn)
+conn.on('ready', async (err) => {
+    if (err) throw err
+    console.log('连接成功');
+    const result = await exec('echo "ywja666" | sudo -S  docker ps -a -q')
+    const dockerIds = result.split('\r')
+    dockerIds.forEach(async (id) => {
+        console.log(id);
+        const data = await exec(`echo "ywja666" | sudo -S  docker logs -f --tail 200 ${id}`)
+        const isError = data.includes('error')
+        console.log(isError, '无',id)
+    })
 }).connect({
     host: '47.111.8.143',
     port: 37219,
